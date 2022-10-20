ebe9fa4b20a115f5f5030b6b9a65dd62826430f6
gharjare <113633267+gharjare@users.noreply.github.com>
2022-10-20 15:41:24 +0530

    Create VendingMachine.cs

---
+++ VendingMachine.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace LogicalProgramming
+{
+    public static class VendingMachine
+    {
+        public static void Calculate()
+        {
+            int money = 2022 ;
+            int[] denominations = new int[] { 1000, 500, 100, 50, 10, 5, 2, 1 };
+            int note = 0;
+
+            while (money > 0)
+            {
+
+                int count = 0;
+
+                Console.WriteLine("Remaining money: " + money);
+
+                while (money >= denominations[note])
+                {
+
+                    money = money - denominations[note];
+
+                    count++;
+
+                }
+
+                Console.WriteLine(denominations[note] + " X " + count);
+
+                note++;
+
+
+
+            }
+        }
+    }
+}
