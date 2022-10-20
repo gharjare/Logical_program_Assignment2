05b5576c62e0af3dc465be77652db021f3e018c4
gharjare <gharjarechetan@gmail.com>
2022-10-20 16:02:51 +0530

    file1

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using System.Web;
+
+namespace LogicalProgramming
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //PerfectNumber.CheckPerfectNumber();
+            //ReverseNumber.Display();
+
+            //ReverseNumber reverseNumber = new ReverseNumber();
+            //reverseNumber.Display();
+
+            //Add add = new Add();
+            //add.Sum();
+
+            //Add add1 = new Add(60, 80);
+            //add1.Sum();
+
+            //FibbonacciSe ries.CheckSeries();
+
+            //Stopwatch stopwatch = new Stopwatch();
+            //stopwatch.Start();
+            //Console.WriteLine("Hi");
+            //stopwatch.Stop();
+            //Console.WriteLine("Time Elapsed : {0}",
+            //        stopwatch.Elapsed);
+
+            //PrimeNumber.checkPrime();
+
+            //VendingMachine.Calculate();
+
+
+            //int month = Convert.ToInt32(args[0]);
+            //int day = Convert.ToInt32(args[1]);
+            //int year = Convert.ToInt32(args[2]);
+            //Day0fWeek.Days(month, day, year);
+
+
+            //FahrenheittoCelsius.Conversion();
+
+            //MonthlyPayment.Payment(args[ 3], args[ 4], args[5]);
+
+
+            SquareRoot.Calculate();
+
+
+
+        }
+                    
+    }
+}
