4fc4bc9a4b62e299a7856ea95b12ae355320d9c5
gharjare <gharjarechetan@gmail.com>
2022-10-20 14:50:55 +0530

    Added day

---
+++ DayofWeek.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    public static class DayofWeek
+    {
+      
+
+        public static void Days(int month, int day, int year)
+        {
+            
+             
+
+            string[] days = { "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday" };
+
+            int dayIndex = Utility.Days(month, day, year);
+
+            Console.WriteLine("days= " + days[dayIndex]);
+        }
+        
+    }
+}
