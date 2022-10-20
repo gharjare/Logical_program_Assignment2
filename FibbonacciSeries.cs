6551ac209ad1c82e1b4c9c52c86d50d7e1cb1cc8
gharjare <gharjarechetan@gmail.com>
2022-10-20 15:57:26 +0530

    series

---
+++ FibbonacciSeries.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    public static class FibbonacciSeries
+    {
+        public static void CheckSeries()
+        {
+            Console.WriteLine("FibbonacciSeries");
+
+
+            int a = 0;
+            int b = 1;
+            int c;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            for (int i = 0; i < 10; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+
+                Console.WriteLine(c);
+
+            }
+        }
+    }
+}
